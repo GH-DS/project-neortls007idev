e4a8479c179a18f2b54d49506e0c5996239bff10
Rahul Gupta <neor007idev@gmail.com>
2021-03-05 03:58:23 -0600

    Update: - ( Engine | RenderDX12 )
    Updated Unit tests

--- src/UnitTests/UnitTests.cpp
+++ src/UnitTests/UnitTests.cpp
@@ -7,13 +7,37 @@
 #include "Engine/Input/InputSystem.hpp"
 #include "Engine/Core/EngineCommon.hpp"
 
+//--------------------------------------------------------------------------------------------------------------------------------------------
+	
+ #include <initguid.h>
+ #include <cguid.h>
+ #include <dxcapi.h>
+ #include <atlcomcli.h>
+ #include <wchar.h>
+ #include <d3dcompiler.h>
+ 
+ //#include <wrl/client.h>
+ //#include <ks.h>
+ #include <dxgi1_4.h>
+ #include <dxcapi.h>
+#include "Engine/RendererDX12/DescriptorHeapDX12.hpp"
+ #pragma comment( lib, "dxcompiler.lib" )  
+ #pragma comment( lib, "d3d12.lib" )      
+ #pragma comment( lib, "dxgi.lib" )  
+ 
+ #ifndef GUID_NULL
+ #define GUID_NULL 0
+ #endif
+//#pragma once
+
+//--------------------------------------------------------------------------------------------------------------------------------------------
+	
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 //--------------------------------------------------------------------------------------------------------------------------------------------
 	
 Window* g_theWindow = nullptr;
 extern RenderContextDX12* g_theRenderer;
-
 
 //--------------------------------------------------------------------------------------------------------------------------------------------
 
@@ -25,12 +49,20 @@ namespace UnitTests
 	
 	TEST_CLASS_INITIALIZE( RenderContextDX12Init )
 	{
+		if ( g_theWindow == nullptr )
+		{
+			g_theWindow = new Window();
+		}
+		Logger::WriteMessage( "Window initialized" );
+
 		if ( g_theRenderer == nullptr )
 		{
 			g_theRenderer = new RenderContextDX12();
 		}
 		Assert::IsNotNull( g_theRenderer , L"Renderer initialization Failed" , LINE_INFO() );
 		Logger::WriteMessage( "Renderer Initialized" );
+
+		g_theRenderer->m_window = g_theWindow;
 	}
 
 	//--------------------------------------------------------------------------------------------------------------------------------------------
@@ -73,11 +105,14 @@ namespace UnitTests
 			Logger::WriteMessage( "DirectX 12 Command Queue Creation Successful" );
 		}	
 //----------------------------------------------------------------------------------------------------------
-		TEST_METHOD( A00040_SwapChainCreation )
+		TEST_METHOD( A00040DescriptorCreation )
 		{
-			m_resourceInit |= g_theRenderer->CreateSwapChain( g_theRenderer->m_commandQueue , g_theRenderer->m_numBackBufferFrames );
-			Assert::AreEqual( m_resourceInit , S_OK , L"Hardware Adapter Creation Check Successful" );
-			Logger::WriteMessage( "DirectX 12 Swap Chain Creation Successful" );
+
+			g_theRenderer->m_RTVDescriptorHeap = new DescriptorHeapDX12( g_theRenderer , D3D12_DESCRIPTOR_HEAP_TYPE_RTV , g_theRenderer->m_numBackBufferFrames );
+			g_theRenderer->m_RTVDescriptorSize = g_theRenderer->m_device->GetDescriptorHandleIncrementSize( D3D12_DESCRIPTOR_HEAP_TYPE_RTV );
+
+			Assert::IsNotNull( g_theRenderer->m_RTVDescriptorHeap , L"Descriptor Heap Creation Failed" );
+			Logger::WriteMessage( "Descriptor Heap Creation Successful" );
 		}
 	};
 }
